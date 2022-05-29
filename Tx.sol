e75fd38d3afa44bbfc194945b162fcab75941050
Ali Bahaari <alibahaari1997@gmail.com>
2022-05-29 19:46:13 +0430

    Completed

---
+++ Tx.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Transaction {
+  address public owner;
+
+  struct Funder {
+    address sender;
+    uint256 value;
+  }
+  Funder[] public funders;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function withdraw() public payable onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+
+    delete funders;
+  }
+
+  function fund() public payable {
+    require(msg.value == 100000000000000000);
+    funders.push(Funder({sender: msg.sender, value: msg.value}));
+  }
+}
